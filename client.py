7a39fff261c1ec5f0c9943ce10bc327ae983fd91
ddrost@techfak.uni-bielefeld.de <ddrost@techfak.uni-bielefeld.de>
2018-06-19 11:44:13 +0200

    Antwort vom Server funktioniert. Einlesen von ZipFiles begonnen

--- client.py
+++ client.py
@@ -3,8 +3,10 @@ import socket
 import util
 import sys
 
+
 host = "localhost"
-port = 5018
+port = 5009
+EOT = "EOT"
 
 identifier = sys.argv[1]
 
@@ -21,16 +23,16 @@ class Client():
 		infoString = util.DicToString(myInfo)
 		print("Client: Sending my info: ",infoString)
 		s.send(bytes(infoString,"utf-8"))
-		#s.close()
-		#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-		#s.connect((host,port))
-		answer = ""
-		while True:
-			inp = s.recv(1)
-			answer += inp.decode("utf-8")
-			if(len(inp) == 0):
-				break
-		print("answer",answer)
+		print("Sending", EOT)
+		s.send(bytes(EOT,"utf-8"))
+		answer = s.recv(1).decode("utf-8")
+		print("Server answered with", answer)
+		#if(answer == "1"):
+		#	print("poor :(")
+		#elif(answer == "0"):
+		#	print("yeah :)")
+
+		#print("answer",answer)
 		s.close()
 		return answer
 
@@ -39,10 +41,10 @@ class Client():
 		s.connect((host,port))
 		heartbeat = {"TYPE":"1","ID":identifier}
 		heartbeatString = util.DicToString(heartbeat)
+		print("Sending a message that is", heartbeatString)
 		s.send(bytes(heartbeatString,"utf-8"))
-		s.close()
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-		s.connect((host,port))
+		print("Sending", EOT)
+		s.send(bytes(EOT,"utf-8"))
 		answer = s.recv(1).decode("utf-8")
 		s.close()
 
@@ -53,11 +55,12 @@ class Client():
 		s.connect((host,port))
 		question = {"ID":identifier,"TYPE":"2"}
 		questionAsString = util.DicToString(question)
+		print("sending ",questionAsString)
 		s.send(bytes(questionAsString,"utf-8"))
-		s.close()
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-		s.connect((host,port))
+		print("sending ",EOT)
+		s.send(bytes(EOT,"utf-8"))
 		answer = s.recv(1).decode("utf-8")
+		print(answer)
 		s.close()
 		return answer
 
@@ -65,11 +68,11 @@ client = Client(identifier,host,port)
 while True:
 	i = input(">")
 	if(i == "register"):
-		print(client.register())
+		print("registered. answer: ",client.register())
 	elif(i == "heartbeat"):
-		print(client.heartbeat())
+		print("heartbeat. answer: ",client.heartbeat())
 	elif(i == "ask"):
-		print(client.ask())
+		print("asking. answer: ",client.ask())
 	elif(i == "quit"):
 		sys.exit()
 	
--- server.py
+++ server.py
@@ -4,10 +4,12 @@ import time
 import util
 from threading import Thread
 import sys
+from zipfile import ZipFile
 
 
 host = "localhost"
-port = 5018
+port = 5009
+EOT = "EOT"
 
 
 
@@ -16,21 +18,38 @@ class Server():
 
 
 	def __init__(self):
+		"""
+		Initialises the class (guess what...).
+		Sets its variables, among these are:
+		All_Clients: a Dict containing all clients of the form {ID:Info} with Info = {GPU:XY,RAM:100...}
+		Packages: A list of all Packages. Listentries are Dictionarys of the form {Name:Blub, URL:Bla, version:42...}
+		"""
 		#A Dic containing all clients Id -> Cl where Cl is a Dic itself containing
 		#Hostname,Ip, etc
 		self.All_Clients = {}
+		self.Packages = list()
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 		self.s.bind((host,port))
 		self.inSocket = None
 		self.addr = None
 		self.listeningThread = None
 
+		self.Packages.append({"Name":"Ein Package", "URL":"www.google.com", "Version":"42"})
+
 	def startListeningThread(self):
+		"""
+		Starts the thread that listens to clients. This is done in a seperate thread so the server can still
+		interact with the user at the same time.
+		"""
 		self.listeningThread = Thread(target = self.listen)
 		self.listeningThread.daemon = True	#so it stoppes correctly as the server shuts down
 		self.listeningThread.start()
 
 	def listen(self):
+		"""
+		Listens with s.listen until a connection is there.
+		Calls waitForIncom, as soon as a connection is established.
+		"""
 		print("Server: Start listening...")
 		self.s.listen(socket.SOMAXCONN)
 		self.inSocket, self.addr = self.s.accept()
@@ -38,25 +57,48 @@ class Server():
 		self.waitForIncom()
 
 	def send(self,message):
+		"""
+		sends the message via the inSocket.
+		"""
 		print("Server: Sending the message ",message)
-		time.sleep(2)
-		self.inSocket.send(bytes(message,"utf-8"))
+		if(len(message) == 1):
+			self.inSocket.send(bytes(message,"utf-8"))
+		else:
+			self.inSocket.send(bytes(message + EOT,"utf-8"))
 
 
 	def waitForIncom(self):
+		"""
+		recieves data. All Data sent has to end with the EOT.
+		Calls ExtractInfoFromString afterwards to handle the input.
+		"""
 		recievedBytes = ""
 		while True:
 			#time.sleep(1)
 			#print("Server: Waiting for Incom...")
 			b = self.inSocket.recv(10)
 			recievedBytes += b.decode("utf-8")
+			print(b)
+			print(recievedBytes)
+			print(EOT in recievedBytes)
 			if(len(b) == 0):
+				break
+			if(EOT in recievedBytes):
+				recievedBytes = recievedBytes[:-3]	#cut the EOT
 				break
 		print("Server: Recieved a Message that is: \n",recievedBytes)
 		self.ExtractInfoFromString(recievedBytes)
 		self.startListeningThread()
 
 	def ExtractInfoFromString(self,St):
+		"""
+		Handles the input St accordingly to its Type.
+		Uses util.StringToDic to translate the String St to a Dictionary.
+		The dictionary conaints the key TYPE that specifies, what the sent data wants to do:
+		TYPE 0 : register
+		TYPE 1 : heartbeat
+		TYPE 2 : question for packages
+		"""
 		incom = util.StringToDic(St)
 		if(incom["TYPE"] == "0"):#0 register
 			if(incom["ID"] not in self.All_Clients):
@@ -68,12 +110,35 @@ class Server():
 			else:
 				self.send("1")
 		elif(incom["TYPE"] == "1"): #1 heartbeat
-			self.All_Clients[incom["ID"]].update({"Timestamp":time.asctime()})
-			self.send("0")
+			if(incom["ID"] in self.All_Clients):
+				self.All_Clients[incom["ID"]].update({"Timestamp":time.asctime()})
+				self.send("0")
+			else:
+				self.send("1")
 		elif(incom["TYPE"] == "2"): #2 anfrage
-			pass
-			self.send("0")
+			updates = self.searchForUpdates(incom)
+			if(updates == 1):
+				self.send("1")
+			else:
+				self.send("0")
+				for pack in updates:
+					self.send(util.DicToString(pack))
 
+	def readInPackage(self, name):
+		print("reading in packages...")
+		ret = {}
+		package = ZipFile(name,"r")
+		for member in package.namelist():
+			ret.update({member:package.open(member,"r").read()})
+		print(ret)
+			
+
+	def searchForUpdates(self, clientDic):
+		"""
+		searches for whether there are packageupdates available for the client specified in clientDic. 
+		"""
+		self.readInPackage("One_Package.zip")
+		return self.Packages
 
 
 	def registerClient(self,identifier,client):
